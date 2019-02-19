d93dc827dd47f0caca250c561adf95f12d42b344
clementine contat <clementine.contat@gmail.com>
2019-02-19 12:59:31 +0000

    seeded the db with 10 fake articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  Article.create(
+    title: Faker::Lorem.words(4),
+    content: Faker::Lorem.paragraph(2)
+  )
+end
+puts 'Finished!'
