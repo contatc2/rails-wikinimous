9075e398548422bab9e0b7a0cad33b12dc3d1716
clementine contat <clementine.contat@gmail.com>
2019-02-19 13:00:35 +0000

    added a controller articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
